49be8f0195024b5f0148a690b60c8a5cf893d9dc
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-06 15:51:57 -0300

    exercicios 2 e 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,8 +1,31 @@
-const a = 10;
+/*const a = 10;
 const b= 5;
 
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
-console.log(a % b);
+console.log(a % b);*/
+
+/*const n1 = 5;
+const n2 = 7;
+
+if(n1 > n2) {
+    console.log(n1);
+}
+else {
+    console.log(n2);
+}*/
+
+/*const n1 = 21;
+const n2 = 19;
+const n3 = 11;
+
+if(n1 > n2 && n1 > n3) {
+    console.log(n1);
+} else if(n2 > n1 && n2 > n3) {
+    console.log(n2);
+} else if(n3 > n1 && n3 > n2) {
+    console.log(n3);
+}*/
+
