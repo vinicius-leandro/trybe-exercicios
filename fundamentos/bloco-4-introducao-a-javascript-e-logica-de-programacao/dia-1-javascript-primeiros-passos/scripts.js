03c02d92a4eaa625794962e8b25661408593f885
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-06 16:51:59 -0300

    exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -56,7 +56,8 @@ if(a1 < 0 || a2 < 0 || a3 < 0) {
     console.log(false);
 }*/
 
-let peçaxadrez = 'peao';
+/* 6-
+let peçaxadrez = 'Torre';
 
 switch(peçaxadrez.toLowerCase()) {
     case 'rei':
@@ -79,5 +80,23 @@ switch(peçaxadrez.toLowerCase()) {
         break;
     default:
         console.log('Peça inválida!');
+}*/
+
+let nota = 89;
+
+if(nota >= 90 && nota <=100) {
+    console.log('A');
+} else if(nota >= 80 && nota <90) {
+    console.log('B');
+} else if(nota >= 70 && nota <80) {
+    console.log('C');
+} else if(nota >= 60 && nota <70) {
+    console.log('D');
+} else if(nota >= 50 && nota <60) {
+    console.log('E');
+} else if(nota < 50 && nota >= 0) {
+    console.log('F');
+} else {
+    console.log('Nota invalida');
 }
 
