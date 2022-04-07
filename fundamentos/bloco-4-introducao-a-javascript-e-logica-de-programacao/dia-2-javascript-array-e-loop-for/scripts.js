5cf32ef80d4e37489be237e796f63219ad74b875
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-07 15:43:12 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,17 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = null;
-let div = null;
-let media = null;
+let maior = null;
 
 for(let cont of numbers) {
-    sum = sum + cont;
-    div += 1;
+    if(cont > maior) {
+        maior = cont;
+    }
 }
 
-media = sum / div;
-
-if(media > 20) {
-    console.log('Valor maior que 20');
-} else {
-    console.log('Valor menor ou igual a 20');
-}
+console.log(maior);
