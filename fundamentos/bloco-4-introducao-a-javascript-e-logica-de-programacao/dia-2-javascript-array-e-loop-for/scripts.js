72696f148f988a87f3ffb39fcd13e14edf7dd8e2
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-07 15:36:44 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,9 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = null;
+let div = null;
 
 for(let cont of numbers) {
     sum = sum + cont;
-    
+    div += 1;
 }
 
-console.log(sum);
+console.log(sum/div);
