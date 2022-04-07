cadd9574ca3dde332c7f8d7528f497ea3707d395
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-07 17:44:06 -0300

    Exercicio bonus 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-bonus/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-bonus/scripts.js
@@ -1,15 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let newNumbers = [];
+let mult = null;
 
 
 for (let index = 1; index < numbers.length; index += 1) {
     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (numbers[index] > numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
-      }
+        mult = numbers[index] * numbers[secondIndex];
+    }
+    newNumbers.push(mult);
+    if(index == numbers.length - 1) {
+        mult = numbers[index] *  2;
+        newNumbers.push(mult);
     }
 }
 
-console.log(numbers)
+
+console.log(newNumbers);
