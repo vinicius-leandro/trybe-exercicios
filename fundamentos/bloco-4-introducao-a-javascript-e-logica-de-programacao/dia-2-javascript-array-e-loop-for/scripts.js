86234c9e1837f4a4aa72b8c42f3742492d59ef08
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-07 16:05:17 -0300

    exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,14 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impar = null;
+let menor = numbers[0];
 
 for(let cont of numbers) {
-    if(cont % 2 == 1) {
-        impar += 1;
+    if(cont < menor) {
+        menor = cont;
     } 
 }
-
-if(impar == 0){
-    console.log('Nenhm valor impar encontrado');
-} else {
-    console.log('existem ' + impar + ' numeros ímpares');
-}
+console.log(menor);
