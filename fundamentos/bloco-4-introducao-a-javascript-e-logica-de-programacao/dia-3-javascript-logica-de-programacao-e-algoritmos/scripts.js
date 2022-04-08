15a4209bb44e26b0aa12e249d1cc06c15ac52cbe
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-08 15:55:23 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -9,3 +9,10 @@ for (let index = 1; index <= fat; index += 1) {
 console.log(mult); */
 
 
+/* 2 -
+let word = 'tryber';
+
+
+for(let i = word.length - 1; i >= 0; i -= 1){
+    console.log(word[i]);
+} */
