30120dc4feab1d5c363e10ddff7d34f549b81f0c
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-11 15:39:55 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-e-for-in/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-e-for-in/scripts.js
@@ -2,6 +2,8 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
-  console.log('Bem vinda, ' + info.personagem);
+info['recorrente'] = 'Sim';
+
+console.log(info);
