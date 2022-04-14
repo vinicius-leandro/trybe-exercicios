7766fde8b2e41dd1a85cafb23061cc9f6d885c33
vinicius leandro <leandrocostavinicius@gmail.com>
2022-04-14 19:04:28 -0300

    exercicio 8

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/scripts.js
@@ -26,3 +26,13 @@ let img = document.createElement('img');
 img.className = 'small-image';
 img.src = 'https://picsum.photos/200';
 secondSection.appendChild(img);
+
+let liItems = ['um', 'dois', 'tres', 
+'quatro', 'cinco', 'seis', 'sete', 
+'oito', 'nove', 'dez'];
+let ul = document.createElement('ul');
+for (index = 1; index < liItems.length; index += 1) {
+    let li = document.createElement('li');
+    li.innerHTML = liItems[index];
+    thirdSection.appendChild(li);
+}
