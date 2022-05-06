281c7e88f6f1ab556016a247ced9c40ec192edb6
vinicius leandro <leandrocostavinicius@gmail.com>
2022-05-06 17:14:02 -0300

    Parte 2 exercicios 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,4 @@
-const testingScope = escopo => {
+const (testingScope) = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
@@ -13,8 +13,43 @@ testingScope(false);
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// const arraySorted = array => `${array[5]},${array[1]},${array[2]},${array[4]},${array[3]},${array[0]}`
+// const arraySorted = (array) => `${array[5]},${array[1]},${array[2]},${array[4]},${array[3]},${array[0]}`
 
-const arraySorted = array => array.sort((a, b) => a - b);
+const arraySorted = (array) => array.sort((a, b) => a - b);
 
 console.log(arraySorted(oddsAndEvens));
+
+parte 2
+
+let skills = ['HTML', 'CSS', 'JavaScript', 'Git', 'Github'];
+
+const alteraString = (palavraEscolhida) => {
+  let frase = 'Eu sou o mais x !';
+  let fraseSeparada = frase.split(' ');
+  let novaFrase = '';
+  for (let index = 0; index < fraseSeparada.length; index += 1) {
+    if (fraseSeparada[index] === 'x') {
+      fraseSeparada.splice(index, 1);
+      fraseSeparada.splice(index, 0, palavraEscolhida);
+    }
+  }
+  for (palavras of fraseSeparada) {
+    novaFrase += `${palavras} `;
+  }
+  return novaFrase;
+}
+
+console.log(alteraString('Bebeto'));
+
+const skillsString = (string) => {
+  const sortedSkills = skills.sort();
+  const separatedString = string.split(' ');
+  const newString = separatedString.concat(sortedSkills);
+  let novaFrase = '';
+  for (palavras of newString) {
+    novaFrase += `${palavras} `;
+  }
+  console.log(novaFrase);
+}
+
+skillsString(alteraString('Bebeto'));
